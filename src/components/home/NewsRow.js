a7be3e87c994cdc1d33098d3545d9b041200697d
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2022-08-12 09:14:45 +0800

    edit a tag

--- src/components/home/NewsRow.js
+++ src/components/home/NewsRow.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-
 
 const NewsRow = ({article}) => {
     return (
-        <a className="flex margin-bot-md cursor-pointer" href={article.link} target="_blank">
+        <a className="flex margin-bot-md cursor-pointer" href={article.link} target="_blank" rel="noreferrer">
             <img src={article.img} alt={article.title} className="article-img"/>
             <div>
                 <p className="subheading-2">{article.title}</p>
